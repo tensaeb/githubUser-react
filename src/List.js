754c52a5b9a4e13b1d419164a6d77f4b6d0a27f5
Tensae Befekadu <tensae.befekadu@kimetrica.com>
2021-02-16 17:49:46 +0300

    second edit

---
+++ src/List.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+
+const api = 'https://api.github.com/users';
+
+const List = () => {
+    const [users, setUsers] = useState([])
+    const getUser = async () => {
+        const response = await fetch(api)
+        const users = await response.json()
+        setUsers(users)
+    }
+
+    useEffect(() => {
+        getUser()
+    }, [])
+
+
+    return (
+        <div>
+            <h2 id='title'>Github Users</h2>
+            <ul className='users'>
+                {users.map((user) => {
+                    const { login, id, avatar_url, html_url } = user
+                    return (
+                        <li key={id}>
+                            <img src={avatar_url} alt={login} />
+                            <div>
+                                <h4>{login}</h4>
+                                <a href={html_url}> Profile </a>
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default List
